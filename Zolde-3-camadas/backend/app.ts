44239053eeacf747d8e4f207018fc72be1625c94
Rogério Marinho <46401377+dead-parrot@users.noreply.github.com>
2023-06-17 19:38:32 -0300

    Update app.ts

--- Zolde-3-camadas/backend/app.ts
+++ Zolde-3-camadas/backend/app.ts
@@ -6,6 +6,7 @@ require("dotenv/config")
 
 const connection = process.env.DB_CONNECTION || '';
 const swaggerUi = require('swagger-ui-express');
+const cors = require("cors");
 
 mongoose.connect(connection, { useNewUrlParser: true } as ConnectOptions)
   .then(() => console.log('Conectado ao MongoDB'))
@@ -14,6 +15,7 @@ mongoose.connect(connection, { useNewUrlParser: true } as ConnectOptions)
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(cors)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(3333, () => console.log("Server is running on 3333"));
